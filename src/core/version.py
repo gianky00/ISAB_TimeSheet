34bd80568a502c05e142625f01cb276152fad43e
00gianky00 <lavoro.allegretti@gmail.com>
2025-12-17 16:17:23 +0100

    add

--- src/core/version.py
+++ src/core/version.py
@@ -2,7 +2,7 @@
 Bot TS - Versione
 Sistema di automazione per portale ISAB
 """
-__version__ = "1.0.3"
+__version__ = "1.0.5"
 __app_name__ = "Bot TS"
 UPDATE_URL = "https://bot-ts.netlify.app/version.json"
 APP_NAME = __app_name__  # Alias per compatibilità
