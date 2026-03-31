8813c13b17667b1b4f6bf8546ee9f5876451f926
00gianky00 <lavoro.allegretti@gmail.com>
2026-03-31 10:00:32 +0200

    chore: release v1.41.0 [auto]

--- src/core/version.py
+++ src/core/version.py
@@ -3,7 +3,7 @@ SyncroJob - Versione
 Sistema di automazione per portale ISAB
 """
 
-__version__ = "1.40.3"
+__version__ = "1.41.0"
 __app_name__ = "SyncroJob"
 UPDATE_URL = "https://projectjob-bot.netlify.app/version.json"
 NETWORK_UPDATE_PATH = r"\\192.168.11.251\Condivisa\ALLEGRETTI\applicazioni\SyncroJob\Setup"
